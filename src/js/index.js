949928fcfd4c753350424a3fd321b687bc651f15
Mario <magabor6@gmail.com>
2020-04-02 20:59:08 +0200

    Working on table

--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,64 @@ function onCheck(event){
             checks[i].checked = false;
     }
 }
+function formatter(value,row,index,field){
+    return `<input type="checkbox"  value=${value} />`;
+}
+function detailFormatter(index, row) {
+    return 'lalalal';
+}
+window.operateEvents  = {
+    'click input': function (event,value,row,index){
+        row.equipped = !value;
+    }
+}
+const columns = [
+    {
+        field: 'portNo',
+        title: 'Port #'
+    },{
+        field: 'portID',
+        title: 'Port Id'
+    },{
+        field: 'equipped',
+        title: 'Equipped',
+        formatter: formatter,
+        events: window.operateEvents
+    }];
+
+$('#table').bootstrapTable({
+    columns: columns,
+    detailView: true,
+    //detailFormatter: detailFormatter
+});
+
+$('#addBtn').on('click',function (){
+    const data = getLastRow();
+    const lastPortNo = data.length ? data[0].portNo : 0;
+    let newrow = newRow(lastPortNo + 1);
+    $('#table').bootstrapTable('append',newrow);
+})
+$('#delBtn').on('click',function (){
+    const data = getLastRow();
+    const lastPortNo = data.length ? data[0].portNo : 0;
+    $('#table').bootstrapTable('remove',{
+        field:'portNo',
+        values: [lastPortNo]
+    });
+})
+
+function getLastRow() {
+    var prevData = $('#table').bootstrapTable('getData');
+    return prevData.slice(-1);
+}
+
+function newRow(portNo){
+    return [{
+        portNo: portNo,
+        portID: portNo,
+        equipped: false
+    }];
+}
+
 
 
